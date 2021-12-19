bcf2a14c3fa76b355bc7621c17044b26250a575e
WilliamKRike <rikeisepic@gmail.com>
2021-12-19 07:08:02 -0500

    IT WORKS LOL

--- commands/playFile.js
+++ commands/playFile.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, getVoiceConnection, entersState } = require('@discordjs/voice');
 const { createReadStream } = require('fs');
-const { createAudioResource, AudioPlayerStatus, createAudioPlayer, NoSubscriberBehavior, VoiceConnectionStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioResource, createAudioPlayer } = require('@discordjs/voice');
 // const { Client, Intents } = require('discord.js');
 // const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
@@ -10,67 +9,40 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('playfile')
 		.setDescription('play file to current channel'),
-	execute(message) {
+	async execute(message) {
 		message.reply('Joining channel...');
-		joinVoiceChannel({
+		const connection = joinVoiceChannel({
 			channelId: message.member.voice.channel.id,
 			guildId: message.guild.id,
 			adapterCreator: message.guild.voiceAdapterCreator,
 		});
-		const connection = getVoiceConnection(message.member.voice.channel.id);
+		//	const connection = getVoiceConnection(message.member.voice.channel.id);
 		// After joining, create an audio player
-		const player = createAudioPlayer({
-			behaviors: {
-				noSubscriber: NoSubscriberBehavior.Pause,
-			},
-		});
-		// play pre-established file!
-		player.play(resource);
-		console.log('Playing song!');
+		const player = createAudioPlayer();
+		connection.subscribe(player);
 
-		// check for error again ??
-		player.on('error', error => {
-			console.error('Error:', error.message, 'with track', error.resource.metadata.title);
-		});
-
-		// because FFMPEG is for chuds
-		const resource = createAudioResource(createReadStream('./snestownsims.mp3'), {
+		// initialize the file
+		const resource = createAudioResource(createReadStream('./fart.mp3'), {
 			inlineVolume: true,
 			metadata: {
 				title: 'test song',
 			},
 		});
 
-		// eslint-disable-next-line no-unused-vars
-		player.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-			try {
-				await Promise.race([
-					entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-					entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-				]);
-				// Seems to be reconnecting to a new channel - ignore disconnect
-			}
-			catch (error) {
-				// Seems to be a real disconnect which SHOULDN'T be recovered from
-				connection.destroy();
-			}
+		// play pre-established file!
+
+		console.log('Playing song!');
+		connection.on('stateChange', (oldState, newState) => {
+			console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
 		});
-
-
-		// is the player playing?
-		player.on(AudioPlayerStatus.Playing, () => {
-			console.log('The audio player has started playing!');
+		player.on('stateChange', (oldState, newState) => {
+			console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
 		});
+		console.log('Bot\'s up and runnin\'!');
+		player.play(resource);
 
-		// is it idle?
-		player.on(AudioPlayerStatus.Idle, () => {
-			console.log('its idle!!!');
-			// player.play(getNextResource());
-		});
-
-		// is it broken?
+		// check for error again ??
 		player.on('error', error => {
-			console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
-			// player.play(getNextResource());
+			console.error('Error:', error.message, 'with track', error.resource.metadata.title);
 		});
 	} };
