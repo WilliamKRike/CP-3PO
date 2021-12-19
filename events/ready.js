f1a57c97e8b2cf922ec01e87d84fbbc65bbfcc11
Kai Nobusada <knobusada@digipen.edu>
2021-12-19 01:29:09 -0800

    Added Event changes

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,9 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		// debugging set listener
+		client.on('guildMemberAdd', console.log);
+
 	},
 };
