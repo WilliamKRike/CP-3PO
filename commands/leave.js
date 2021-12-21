9cd99eb7ecdabc50f67eef9a1e69d9a3bd8c93bf
WilliamKRike <rikeisepic@gmail.com>
2021-12-21 01:31:41 -0500

    connecttest?

--- commands/leave.js
+++ commands/leave.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, getVoiceConnections, createAudioPlayer } = require('@discordjs/voice');
+const { getVoiceConnections, createAudioPlayer } = require('@discordjs/voice');
 // const { Client } = require('discord.js');
 
 module.exports = {
@@ -7,21 +7,19 @@ module.exports = {
 		.setName('leave')
 		.setDescription('leave the current channel'),
 	execute(message) {
-		console.log('LEAVING\nLEAVING\nLEAVING');
-		console.log(getVoiceConnections());
-		console.log('channel ID =');
-		console.log();
 
 		/* if (getVoiceConnections().channelId == undefined) {
 			console.log('not in voice channel, will do nothing');
 			message.reply('Not in a voice channel!');
 		}
 		else {
-			const connection = getVoiceConnections(message.guild.id);
-			message.reply('Leaving channel...');
-			const player = createAudioPlayer();
-			player.stop();
-			connection.destroy();
 		}
-*/
+		*/
+		const connection = getVoiceConnections(message.guild.id);
+		message.reply('Leaving channel...');
+		const player = createAudioPlayer();
+		player.stop();
+		connection.destroy();
+
+
 	} };
