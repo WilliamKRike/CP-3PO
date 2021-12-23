34dc5c929dd83032741ecd038b814c9d31e7069b
WilliamKRike <rikeisepic@gmail.com>
2021-12-23 07:54:46 -0500

    good for today verything works

--- commands/leave.js
+++ commands/leave.js
@@ -1,14 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { getVoiceConnections, createAudioPlayer } = require('@discordjs/voice');
-
+const { getVoiceConnection } = require('@discordjs/voice');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('leave')
 		.setDescription('leave the current channel'),
 	execute(message) {
-		let connection = getVoiceConnections(message.guild.id);
-		console.log(message.guild.me.voice.channelId);
+		const connection = getVoiceConnection(message.guildId);
 		// if it's already not in a lobby do nothing
 		if (message.guild.me.voice.channelId == null) {
 			message.reply('The bot is not connected to any channel, you dip');
@@ -18,16 +16,8 @@ module.exports = {
 			message.reply(`The bot is in a different voice channel, ${message.guild.me.voice.channel}, you dip`);
 		}
 		else {
-			while (connection == undefined || null) {
-				console.log(connection);
-				connection = getVoiceConnections(message.guild.id);
-				break;
-			}
 			message.reply('Leaving channel...');
-			const player = createAudioPlayer();
-			player.stop();
-			connection.destroy();
+			connection.disconnect();
+			console.log(message.guild.me.voice.channelId);
 		}
-
-
 	} };
--- commands/playYouTube.js
+++ commands/playYouTube.js
@@ -1,8 +1,6 @@
-const { createReadStream } = require('fs');
-const fs = require('fs');
-const ytdl = require('ytdl-core');
+const play = require('play-dl');
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel, createAudioResource, createAudioPlayer, AudioPlayerStatus } = require('@discordjs/voice');
 // TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
@@ -10,56 +8,45 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
 		.setDescription('play youtube')
-		.addStringOption(option => option.setName('link')
-			.setDescription('Enter link here')
+		.addStringOption(option => option.setName('query')
+			.setDescription('Enter terms or link here')
 			.setRequired(true)),
 	async execute(message) {
-		const link = message.options.getString('link');
-		message.reply(`Playing your video!\nLink: ${link}`);
-		// download file
-		await ytdl(`${link}`)
-			.pipe(fs.createWriteStream('video.mp3'));
-
-		// initialize the file
-		const connection = await joinVoiceChannel({
-			channelId: message.member.voice.channel.id,
-			guildId: message.guild.id,
+		message.reply('Playing your song!');
+		const connection = joinVoiceChannel({
+			channelId : message.member.voice.channel.id,
+			guildId : message.guild.id,
 			adapterCreator: message.guild.voiceAdapterCreator,
 		});
 
-		// After joining, create an audio player
-		const player = await createAudioPlayer();
-		await connection.subscribe(player);
-		playResource();
+		const args = message.options.getString('query');
+		const yt_info = await play.search(args, { limit:1 });
+		const stream = await play.stream(yt_info[0].url);
 
-		// play pre-established file!
+		/*
+        OR if you want to get info about youtube link and then stream it
+        let yt_info = await play.video_info(args)
+        console.log(yt_info.video_details.title)
+        let stream = await play.stream_from_info(yt_info)
+        */
+
+		const resource = createAudioResource(stream.stream, {
+			inputType : stream.type,
+		});
+		const player = createAudioPlayer({
+			behaviors: {
+				noSubscriber: NoSubscriberBehavior.Play,
+			},
+		});
+		player.play(resource);
+		connection.subscribe(player);
+
+		console.log('Playing song!');
 		connection.on('stateChange', (oldState, newState) => {
 			console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
 		});
 		player.on('stateChange', (oldState, newState) => {
 			console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
-		});
-		player.on(AudioPlayerStatus.Idle, () => {
-			console.log('IDLE WARN\nIdle for some reason!');
-			playResource();
-		});
-
-		console.log('Bot\'s up and runnin\'!');
-		function playResource() {
-			const resource = createAudioResource(createReadStream('./video.mp3'), {
-				inlineVolume: true,
-				metadata: {
-					title: 'file',
-				},
-			});
-			player.play(resource);
-			console.log('Playing song!');
-		}
-
-
-		// check for error again ??
-		player.on('error', error => {
-			console.error('Error:', error.message, 'with track', error.resource.metadata.title);
 		});
 	},
 };
