66f472fecf7399352e0141536095ffdbbfbd4f5e
WilliamKRike <rikeisepic@gmail.com>
2021-12-27 21:59:06 -0500

    e

--- commands/playYouTube.js
+++ commands/playYouTube.js
@@ -1,23 +1,42 @@
 const play = require('play-dl');
-const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior } = require('@discordjs/voice');
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior, AudioPlayerStatus } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const queue = new Map();
 // TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
 // TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('play youtube')
+		.setDescription('play youtube audio')
 		.addStringOption(option => option.setName('query')
 			.setDescription('Enter terms or link here')
 			.setRequired(true)),
 	async execute(message) {
+		const serverQueue = queue.get(message.guild.id);
 		message.reply('Playing your song!');
 		const connection = joinVoiceChannel({
 			channelId : message.member.voice.channel.id,
 			guildId : message.guild.id,
 			adapterCreator: message.guild.voiceAdapterCreator,
 		});
+		// makeQueue
+		if (!serverQueue) {
+			const queue_constructor = {
+				voiceChannel: message.member.voice.channel.id,
+				textChannel: message.channelId,
+				connection: null,
+				songs: [],
+			};
+			queue.set(message.guild.id, queue_constructor);
+			queue_constructor.songs.push(33);
+			console.log(queue_constructor.songs[0]);
+			video_player(message.guild.id, queue_constructor.songs[0]);
+		}
+		else {
+			queue.songs.push(40);
+			console.log(queue.songs[0]);
+		}
 
 		const args = message.options.getString('query');
 		const yt_info = await play.search(args, { limit:1 });
@@ -33,6 +52,7 @@ module.exports = {
 		const resource = createAudioResource(stream.stream, {
 			inputType : stream.type,
 		});
+
 		const player = createAudioPlayer({
 			behaviors: {
 				noSubscriber: NoSubscriberBehavior.Play,
@@ -49,4 +69,22 @@ module.exports = {
 			console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
 		});
 	},
+};
+
+const video_player = async (guild, song) => {
+	const song_queue = queue.get(guild.id);
+
+	// If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
+	if (!song) {
+		song_queue.voice_channel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	const stream = play.stream();
+	song_queue.connection.play(stream, { seek: 0, volume: 0.5 })
+		.on(AudioPlayerStatus.Idle, () => {
+			song_queue.songs.shift();
+			video_player(guild, song_queue.songs[0]);
+		});
+	await song_queue.text_channel.send(`🎶 Now playing **${song.title}**`);
 };
